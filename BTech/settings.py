177de9e5411eeac3e13917269482cfa1ff4e0817
sluzer <amanisral633@gmail.com>
2022-07-24 07:25:40 +0600

    deploi

--- BTech/settings.py
+++ BTech/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
     'drf_yasg',
     'rest_framework',
     "rest_framework.authtoken",
